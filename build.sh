eb54cd56f033a99d8527e9d0d148f54583f68407
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2019-07-04 08:48:00 +0200

    Clean-up of build scripts and makefiles & support for `asm.js` (not working)

--- build.sh
+++ build.sh
@@ -2,14 +2,19 @@
 
 PYODIDE_ROOT=pyodide-0.12.0
 
+# Compile python
 (
   cd ${PYODIDE_ROOT} && \
   ./run_docker || exit $?
 ) || exit $?
 
+# Move compiled python binaries to `bin` folder
 mkdir -p bin || exit $?
-cp ${PYODIDE_ROOT}/build/python3.7.wasm bin || exit $?
-chmod +x bin/python3.7.wasm || exit $?
+cp ${PYODIDE_ROOT}/build/python3.7.* bin || exit $?
 
+# Set executable bit on WebAssembly python binary
+chmod +x bin/python3.7.wasm
+
+# Update python standard library
 rm -rf lib || exit $?
 cp -R ${PYODIDE_ROOT}/cpython/installs/python-3.7.0/lib . || exit $?
